b4b9edb6d038314289c488c974dcf55ad8424f89
Vincent Dahmen <4dahmen@informatik.uni-hamburg.de>
2015-11-16 09:36:17 +0100

    Blatt fertig aber mehr schlecht als recht ...

--- 04/Blatt04.rkt
+++ 04/Blatt04.rkt
@@ -1,5 +1,14 @@
 #lang racket
 ;; Aufgabe 1.0
+; 1) -> 0
+; 2) -> '0
+; 3) -> 'wieder
+; 4) -> '()
+; 5) -> 'Weichnachtsfest
+; 6) -> '(Listen ganz einfach und)
+; 7) -> '(Paare . auch)
+; 8) -> #t
+; 9) -> #f
 
 
 
@@ -63,7 +72,30 @@
 )
 
 
-
+;; Utils
 ;;; found at rosettacode.org/Repeat_a_string		 
 (define (string-repeat n str)
   (string-append (make-list n str)))
+
+;;; Aufgabe 3
+
+;;; Bei der äußeren Reduktion werden zunächst die äußeren Funktionen nach Definion aufgelöst:
+; (hoch3 (* 3 (+ 1 (hoch3 2))) -> (* (* 3 (+ 1 (hoch3 2)))(* 3 (+ 1 (hoch3 2)))(* 3 (+ 1 (hoch3 2)))
+; (* (* 3 (+ 1 (* 2 2 2))) (* 3 (+ 1 (* 2 2 2))) (* 3 (+ 1 (* 2 2 2))))
+; ...
+; Bei der inneren Reduktion passiert es genau andersherum
+; (hoch3 (* 3 (+ 1 (hoch3 2))) -> (hoch3 (* 3 (+ 1 (* 2 2 2))) -> (hoch3 (* 3 (+ 1 8)) -> (hoch3 (* 3 9)) ->
+; (hoch3 27) -> (* 27 27 27)
+
+; + Racket benutzt die innere Reduktion für die Auswertung von Funktionen
+;   und die aplikative Reduktion für die Auswertung von special form expressions
+
+
+;+ Wenn das Programm ausgeführt wird kommt es unweigerlich zu einem "Buffer-Overflow", das Program
+;  benötigt also zu viel Speicher.
+;  Dies hängt damit zusammen, dass die innere Reduktion die auswertung der Bedingung verhindert.
+;  Statt wie erwartet prüft Racket mit "new-if" nämlich nicht zuerst die Bedingung sondern substituiert
+;  den Funktionsaufruf mit dem Funktionskörper. Da es sich hierbei um einen rekursiven Aufruf handelt,
+;  passiert dies endlich oft.
+;  Genau dieses Phänomen wird durch die applikative Reduktion verhindert, da eine Auswertung von rechts nach
+;  links erzwungen wird.
